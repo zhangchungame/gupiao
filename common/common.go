2d8f0bad518a9da3940a5b7e1f5886e668d80dda
zhangchungame <zhangchungame@163.com>
2017-01-03 23:21:24 +0800

    开始用orm

--- common/common.go
+++ common/common.go
@@ -1,9 +1,22 @@
 package common
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/astaxie/beego/orm"
+	"gupiao/allcode"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func Checkerr(err error) {
 	if err!=nil {
 		fmt.Println(err)
 	}
+}
+
+func OrmInit()  {
+	orm.RegisterModel(new(allcode.Baseinfo))
+
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/gupiao?charset=utf8")
 }
--- test.go
+++ test.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"gupiao/rikxian"
+	"fmt"
+	"github.com/astaxie/beego/orm"
+	"gupiao/common"
 )
 
+func init() {
+	common.OrmInit()
+}
+
 func main() {
-	rikxian.Getallkxian()
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+
+	user := new(Baseinfo)
+	user.Code="123123"
+	user.Name="zc"
+	id,err:=o.Insert(user)
+	fmt.Println(id)
+	fmt.Println(err)
+	var infos []*Baseinfo
+	o.QueryTable("baseinfo").Filter("id__lt", 5).All(&infos)
+	fmt.Println(infos[2])
 }
